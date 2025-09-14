74311df599b2b6f4c659544c5bd7b74f89742e12
Saquib Hassan <hsakib1244@outlook.com>
2025-09-15 00:27:26 +0600

    unfinished

---
+++ Assignment_01/Loops/2.Armstrong_Number_Checker/main.py
@@ -0,0 +1,5 @@
+"""Write a Python program that takes an integer 
+input from the user and checks if it is an Armstrong number (a number that is 
+equal to the sum of its own digits raised to the power of the number of digits) 
+using a for loop."""
+

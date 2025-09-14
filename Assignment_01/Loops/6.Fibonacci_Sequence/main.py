54bca97805698bfc8d4b620115b9988911fbd8e7
Saquib Hassan <hsakib1244@outlook.com>
2025-09-15 04:02:47 +0600

    Done

--- Assignment_01/Loops/6.Fibonacci_Sequence/main.py
+++ Assignment_01/Loops/6.Fibonacci_Sequence/main.py
@@ -1,2 +1,13 @@
 """nput N. Print the first N terms of the Fibonacci 
 sequence. """
+
+num_of_terms = int(input("How many Fibonacci numbers do you want? "))
+
+first = 0
+second = 1
+
+for count in range(num_of_terms):
+    print(first)
+    next_number = first + second  #calculate the next number
+    first = second                #shift the sequence forward
+    second = next_number
